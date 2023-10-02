429ef7d02c30950270c94f60fa65ac16044da573
Dariia <d.o.prybyla@student.khai.edu>
2023-10-02 21:51:58 +0300

    Арифметичні дії

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
+    val a = 5
+    val b = 6
+    val c = 100
+
+    println(a + b - c)
+
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
